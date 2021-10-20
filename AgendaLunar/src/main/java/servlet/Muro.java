e4920d7d5ade47b0a4cdd871910f052bef19ca58
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2021-10-20 14:31:23 -0400

    Agregando el Muro y Blog en jsp

--- AgendaLunar/src/main/java/servlet/Muro.java
+++ AgendaLunar/src/main/java/servlet/Muro.java
@@ -5,8 +5,14 @@
  */
 package servlet;
 
+import Conexion.ConnectionDB;
+import Conexion.ControlDBPublicacion;
+import POJOS.Publicacion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Muro", urlPatterns = {"/Muro"})
 public class Muro extends HttpServlet {
-
+String ID;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -33,56 +39,61 @@ public class Muro extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Muro</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Muro at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+             ID= request.getParameter("ID");
+             System.out.println(ID);
+             System.out.println("JAMESSSSSSSSSSSSSS////////////////");
+             doPost(request,response);
+            //request.setAttribute("USER", USER);
+            //request.getRequestDispatcher("/PagesGerente/CrearGerente.jsp").forward(request, response);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+   
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                System.out.println("JAMESSSSSSSSSSSSSS////////22222////////");
+                List<Publicacion> publicaciones = new ArrayList<>();
+                
+                //establece la conexion a la DB
+                ConnectionDB connect = new ConnectionDB();
+                Connection connection = connect.getConnection();
+                
+                ControlDBPublicacion ControlP=new ControlDBPublicacion(connection);
+                publicaciones = ControlP.getTodasPublicacionesPorNombreUsuario(ID);
+        
+        
+        
+        
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
 }
+
+
+//<%
+//            HttpSession sesionAsociaciones = request.getSession();
+//            boolean correcto = false;
+//            if (sesionAsociaciones.getAttribute("tipo") != null) {
+//                if (sesionAsociaciones.getAttribute("tipo").toString().equalsIgnoreCase("CLIENTE")) {
+//                    correcto = true;
+//                } else {
+//                    response.sendRedirect("home.jsp");
+//                }
+//            } else {
+//                response.sendRedirect("login.jsp");
+//            }
+// %>
